2033933a8b8504c96d68d97005f031eb2e20e3cf
Codekster <hbfabhi86@gmail.com>
2026-02-23 20:50:51 +0530

    [Abhishek Pandey]: Add updated test cases remainign after automated ones

--- src/test/java/com/app/quantitymeasurementapp/QuantityMeasurementApp/UC7TestCases.java
+++ src/test/java/com/app/quantitymeasurementapp/QuantityMeasurementApp/UC7TestCases.java
@@ -1,6 +1,6 @@
 package com.app.quantitymeasurementapp.QuantityMeasurementApp;
 
-//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -65,4 +65,72 @@ public class UC7TestCases {
         return arguments.stream();
     }
 	
+    @Test
+    public void testAddition_ExplicitTargetUnit_Commutativity() {
+        Length length1 = new Length(1.0, Length.LengthUnit.FEET);
+        Length length2 = new Length(12.0, Length.LengthUnit.INCHES);
+        
+        // A + B -> YARDS
+        Length sum1 = QuantityMeasurementApp.demonstrateLengthAddition(length1, length2, Length.LengthUnit.YARDS);
+        // B + A -> YARDS
+        Length sum2 = QuantityMeasurementApp.demonstrateLengthAddition(length2, length1, Length.LengthUnit.YARDS);
+        
+        assertTrue(QuantityMeasurementApp.demonstrateLengthEquality(sum1, sum2), 
+            "Commutativity should hold with explicit target unit");
+    }
+
+    @Test
+    public void testAddition_ExplicitTargetUnit_WithZero() {
+        Length sum = QuantityMeasurementApp.demonstrateLengthAddition(
+            new Length(5.0, Length.LengthUnit.FEET), 
+            new Length(0.0, Length.LengthUnit.INCHES), 
+            Length.LengthUnit.YARDS
+        );
+        // 5.0 Feet + 0.0 Inches = 5.0 Feet. 5.0 Feet / 3.0 = 1.666... (Rounds to 1.67)
+        assertTrue(QuantityMeasurementApp.demonstrateLengthEquality(sum, new Length(1.67, Length.LengthUnit.YARDS)));
+    }
+
+    @Test
+    public void testAddition_ExplicitTargetUnit_NegativeValues() {
+        Length sum = QuantityMeasurementApp.demonstrateLengthAddition(
+            new Length(5.0, Length.LengthUnit.FEET), 
+            new Length(-2.0, Length.LengthUnit.FEET), 
+            Length.LengthUnit.INCHES
+        );
+        // 5 Feet - 2 Feet = 3 Feet = 36 Inches
+        assertTrue(QuantityMeasurementApp.demonstrateLengthEquality(sum, new Length(36.0, Length.LengthUnit.INCHES)));
+    }
+
+    @Test
+    public void testAddition_ExplicitTargetUnit_NullTargetUnit() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            QuantityMeasurementApp.demonstrateLengthAddition(
+                new Length(1.0, Length.LengthUnit.FEET), 
+                new Length(12.0, Length.LengthUnit.INCHES), 
+                null
+            );
+        }, "Should throw IllegalArgumentException when target unit is null");
+    }
+
+    @Test
+    public void testAddition_ExplicitTargetUnit_LargeToSmallScale() {
+        Length sum = QuantityMeasurementApp.demonstrateLengthAddition(
+            new Length(1000.0, Length.LengthUnit.FEET), 
+            new Length(500.0, Length.LengthUnit.FEET), 
+            Length.LengthUnit.INCHES
+        );
+        // 1500 Feet * 12 = 18000 Inches
+        assertTrue(QuantityMeasurementApp.demonstrateLengthEquality(sum, new Length(18000.0, Length.LengthUnit.INCHES)));
+    }
+
+    @Test
+    public void testAddition_ExplicitTargetUnit_SmallToLargeScale() {
+        Length sum = QuantityMeasurementApp.demonstrateLengthAddition(
+            new Length(12.0, Length.LengthUnit.INCHES), 
+            new Length(12.0, Length.LengthUnit.INCHES), 
+            Length.LengthUnit.YARDS
+        );
+        // 24 Inches = 2 Feet = 0.666... Yards (Rounds to 0.67)
+        assertTrue(QuantityMeasurementApp.demonstrateLengthEquality(sum, new Length(0.67, Length.LengthUnit.YARDS)));
+    }
 }
