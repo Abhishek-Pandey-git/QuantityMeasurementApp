f32b4e6c6566c87ca6243038a681cf19f46b5857
Codekster <hbfabhi86@gmail.com>
2026-02-28 11:30:16 +0530

    [Abhishek Pandey]: Add functional interface which says that whether methods are alowed or not

---
+++ src/main/java/com/app/quantitymeasurementapp/QuantityMeasurementApp/SupportsArithmetic.java
@@ -0,0 +1,9 @@
+package com.app.quantitymeasurementapp.QuantityMeasurementApp;
+
+/**
+ * UC14: Functional Interface to dictate if a unit supports arithmetic.
+ */
+@FunctionalInterface
+public interface SupportsArithmetic {
+    boolean isSupported();
+}
