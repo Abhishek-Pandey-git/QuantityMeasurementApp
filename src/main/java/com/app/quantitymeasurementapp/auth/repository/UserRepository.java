65d5fc582b0d0a424042ec4945366fa3e4e55f25
Codekster <hbfabhi86@gmail.com>
2026-03-30 15:26:36 +0530

    [Abhishek Pandey]: UserRepository.java repository for user data access

---
+++ src/main/java/com/app/quantitymeasurementapp/auth/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.app.quantitymeasurementapp.auth.repository;
+
+import com.app.quantitymeasurementapp.auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
